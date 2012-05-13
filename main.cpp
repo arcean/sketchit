a6af807c546463ba2c6af90dd26675a1c0377edb
Tomasz Pieniążek <t.pieniazek@gazeta.pl>
2012-05-13 15:53:39 +0200

    UI fixes.

--- main.cpp
+++ main.cpp
@@ -7,23 +7,24 @@
 #include <MWidgetAction>
 #include <MTextEdit>
 #include <QObject>
+#include <MComponentCache>
 
 #include "mainpage.h"
 #include "window.h"
 
-int main(int argc, char *argv[]){
-    MApplication application(argc, argv);
-    application.setOrganizationName("arcean");
-    application.setOrganizationDomain("arcean.com");
-    application.setApplicationName("SketchIt");
+M_EXPORT int main(int argc, char *argv[]){
+    MApplication* application = MComponentCache::mApplication(argc, argv);
+    application->setOrganizationName("arcean");
+    application->setOrganizationDomain("arcean.com");
+    application->setApplicationName("SketchIt");
 
-    Window window;
+    MApplicationWindow* window = MComponentCache::mApplicationWindow();
     MainPage *mainPage = new MainPage();
-    mainPage->connect(&window, SIGNAL(needToSave()), mainPage, SLOT(wantsToCloseWindow()));
-    window.setMainPage(mainPage);
-    mainPage->appear(&window);
+    mainPage->connect(window, SIGNAL(needToSave()), mainPage, SLOT(wantsToCloseWindow()));
 
-    window.show();
+    mainPage->appear(window);
 
-    return application.exec();
+    window->show();
+
+    return application->exec();
  }
